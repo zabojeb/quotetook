91bc35984805e095d57efa76ccfac8109e189313
zabojeb <128208855+zabojeb@users.noreply.github.com>
2023-11-04 16:31:21 +0300

    Add main files

---
+++ script.js
@@ -0,0 +1,39 @@
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
+const generateButton = document.getElementById('generate-button');
+const categorySelect = document.getElementById('category-select');
+
+async function getQuote() {
+    const selectedCategory = categorySelect.value;
+    const apiKey = process.env.API_KEY; // Получаем ключ API из переменной окружения
+
+    const url = `https://api.api-ninjas.com/v1/quotes?category=${selectedCategory}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': apiKey, // Используем ключ API
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        if (result.length > 0) {
+            const quoteData = result[0];
+            quoteText.textContent = quoteData.quote;
+            quoteAuthor.textContent = quoteData.author;
+        } else {
+            quoteText.textContent = 'No quotes found for this category';
+            quoteAuthor.textContent = 'No Author';
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+generateButton.addEventListener('click', getQuote);
+
+categorySelect.addEventListener('input', getQuote);
+// Загрузка начальной цитаты при загрузке страницы
+getQuote();
